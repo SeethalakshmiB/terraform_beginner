703089d62c2a595ba55ad12aed98a405ea8f97e0
Seethalakshmi <seethacs1142@gmail.com>
2022-01-17 23:37:55 +0530

    Session-3:VPC Setup

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-
-1. provider 
-2. variables & outputs
-3. vpc
+VPC Infra
+1. VPC
+2. IGW
+3. 4 Subnets
+4. Public Route Table (associated 2 public subnets )
+5. NAT --> EIP and public Subnet
+6. Private Route Table (associated 2 private subnets )
--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,6 +1,10 @@
 provider "aws" {
-  version = "3.50.0"
   region  = "us-east-1"
+}
+
+# Declare the data source --> aws_availability_zones.available.names --> list 
+data "aws_availability_zones" "available" {
+  state = "available"
 }
 
 variable "vpc_cidr_block" {
@@ -15,20 +19,135 @@ variable "instance_tenancy_type" {
   default     = "default"
 }
 
-variable "vpc_tags" {
-  type        = map(string)
-  description = "tags to be added for vpc"
-  default = {
-    "Name" = "seetha_vpc_test"
-    "CreatedVia" = "tf"
-  }
-}
-
-
+# VPC
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr_block
   instance_tenancy = var.instance_tenancy_type
 
-  tags = var.vpc_tags
+  tags = {
+    "Name" = "capstone_vpc"
+  }
 }
 
+#Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "capstone_igw"
+  }
+}
+
+# Public_Subnet_1 - us-east-1a
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.1.1.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "capstone_public_subnet_1"
+  }
+}
+
+# Public_Subnet_2 - us-east-1b
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.1.2.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "capstone_public_subnet_2"
+  }
+}
+
+# Private_Subnet_1 - us-east-1a
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.1.3.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "capstone_private_subnet_1"
+  }
+}
+
+# Private_Subnet_2 - us-east-1b
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.1.4.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "capstone_private_subnet_2"
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "capstone_public_rt"
+  }
+}
+
+# Public Route Table - Subnet Associations
+resource "aws_route_table_association" "public_rt_assoc_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Public Route Table - Subnet Associations for public_subnet_2
+resource "aws_route_table_association" "public_rt_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Nat Gateway
+resource "aws_nat_gateway" "nat-gateway" {
+  allocation_id = aws_eip.eip.id
+  connectivity_type = "public"
+  subnet_id         = aws_subnet.public_subnet_1.id
+  tags = {
+    Name = "capstone_nat"
+  }
+}
+
+# Elastic IP
+resource "aws_eip" "eip" {
+  vpc = true
+  tags = {
+    Name = "capstone_eip"
+  }
+}
+
+# Private Route Table 
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat-gateway.id
+  }
+
+  tags = {
+    Name = "capstone_private_rt"
+  }
+}
+
+# Private Route Table - Subnet Associations for private_subnet_1
+resource "aws_route_table_association" "private_rt_assoc_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# Private Route Table - Subnet Associations for private_subnet_2
+resource "aws_route_table_association" "private_rt_assoc_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
