d04b5e68515ed358d7f36317dba1a58fa45cca31
Seethalakshmi <seethacs1142@gmail.com>
2022-01-18 23:40:38 +0530

    Update readme

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ VPC Infra
 4. Public Route Table (associated 2 public subnets )
 5. NAT --> EIP and public Subnet
 6. Private Route Table (associated 2 private subnets )
+
+RDS
+1. Subnet Group (2 private subnets)
+2. Security Group
+3. RDS Instance (Mysql)
--- capstone/vpc.tf
+++ capstone/vpc.tf
@@ -87,32 +87,32 @@ resource "aws_route_table_association" "public_rt_assoc_2" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-// # Nat Gateway
-// resource "aws_nat_gateway" "nat_gateway" {
-//   allocation_id = aws_eip.eip.id
-//   connectivity_type = "public"
-//   subnet_id         = aws_subnet.public_subnet_1.id
-//   tags = {
-//     Name = "${var.project}_nat"
-//   }
-// }
+# Nat Gateway
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.eip.id
+  connectivity_type = "public"
+  subnet_id         = aws_subnet.public_subnet_1.id
+  tags = {
+    Name = "${var.project}_nat"
+  }
+}
 
-// # Elastic IP
-// resource "aws_eip" "eip" {
-//   vpc = true
-//   tags = {
-//     Name = "${var.project}_eip"
-//   }
-// }
+# Elastic IP
+resource "aws_eip" "eip" {
+  vpc = true
+  tags = {
+    Name = "${var.project}_eip"
+  }
+}
 
 # Private Route Table 
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
 
-  // route {
-  //   cidr_block = "0.0.0.0/0"
-  //   gateway_id = aws_nat_gateway.nat_gateway.id
-  // }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway.id
+  }
 
   tags = {
     Name = "${var.project}_private_rt"
